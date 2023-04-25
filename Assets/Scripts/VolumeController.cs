e2a93356167070c8c1fbc3df103b417098f54469
Yu-Brandon <Brandonyu3388@gmail.com>
2023-04-25 16:37:29 -0700

    Main Menu with volume slider
    Main menu along with a options screen where you can play change volume and quit the game.

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider = null;
+
+    public TMP_Text volumeText = null;
+
+    private void Start()
+    {
+        LoadValues();
+    }
+
+    public void VolumeSlider(float number)
+    {
+        volumeText.text = number.ToString("0.0");
+    }
+
+    public void SaveVolumeButton()
+    {
+        float volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volume);
+        LoadValues();
+    }
+
+    void LoadValues()
+    {
+        float volume = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volume;
+        AudioListener.volume = volume;
+    }
+}
