1c1e45c2212cf6fc8ab58fc895215f4e0dc20e17
JordanNewkirk <jordannewkirk@gmail.com>
2023-05-14 22:01:14 -0700

    push changes

--- Assets/Scripts/Zombie/ZombieAI.cs
+++ Assets/Scripts/Zombie/ZombieAI.cs
@@ -5,57 +5,79 @@ public class ZombieAI : MonoBehaviour
 {
     public Transform player;
     public NavMeshAgent agent;
+    public Animator anim;
 
     public float damageToGive = 20f;
 
-    public Animator anim;
+    public float randomMoveInterval = 10f; // Interval for random movements
+    private float randomMoveTimer; // Timer for tracking random movement intervals
+    private bool isRandomMoving; // Flag for indicating random movement
 
-    private bool isChasing = false;
-    public float chaseDistance = 75f;
-
+    private Vector3 randomDestination; // Random destination for movement
 
     void Start()
     {
-        //Debug.Log("Player: " + player.name);
         damageToGive = 20f;
-      
+        randomMoveTimer = randomMoveInterval;
     }
 
     void Update()
     {
         player = GameObject.FindWithTag("Player").transform;
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if(distanceToPlayer <= chaseDistance)
+        // Always move towards the player
+        agent.SetDestination(player.position);
+
+        // Check if it's time for a random movement
+        randomMoveTimer -= Time.deltaTime;
+        if (randomMoveTimer <= 0f)
         {
-            isChasing = true;
-            anim.SetBool("isRunning", true);
-        }
-        else
-        {
-            isChasing = false;
-            anim.SetBool("isRunning", false);
+            // Toggle random movement on/off
+            isRandomMoving = !isRandomMoving;
+
+            if (isRandomMoving)
+            {
+                // Generate a random destination
+                randomDestination = GenerateRandomDestination();
+                agent.SetDestination(randomDestination);
+                anim.SetBool("isRunning", true);
+            }
+            else
+            {
+                // Resume moving towards the player
+                agent.SetDestination(player.position);
+                anim.SetBool("isRunning", false);
+            }
+
+            // Reset the random movement timer
+            randomMoveTimer = Random.Range(randomMoveInterval * 0.5f, randomMoveInterval * 1.5f);
         }
 
-        if(isChasing)
-        {
-            agent.SetDestination(player.position);
-        }
-        //Debug.Log("Zombies heading to player position: " + player.position);
-
+        // Update animator parameter for speed
+        float speed = agent.velocity.magnitude;
+        anim.SetFloat("Speed", speed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             Vector3 hitDirection = other.transform.position - transform.position;
             hitDirection = hitDirection.normalized;
 
             FindObjectOfType<HealthManager>().hurtPlayer(damageToGive, hitDirection);
         }
-        
+    }
+
+    // Generate a random destination within a certain range
+    private Vector3 GenerateRandomDestination()
+    {
+        float randomX = Random.Range(-50f, 50f);
+        float randomZ = Random.Range(-50f, 50f);
+        Vector3 randomDirection = new Vector3(randomX, 0f, randomZ);
+        return player.position + randomDirection;
     }
 }
+
+
 
